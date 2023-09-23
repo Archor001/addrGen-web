49ac0c3097d4b96f8df6c8bf58eb2edea9e40cce
wolverine2714 <m202271789@hust.edu.cn>
2023-09-23 16:57:37 +0800

    feat: better ISP page

--- src/pages/admin/AddressISP.vue
+++ src/pages/admin/AddressISP.vue
@@ -1,5 +1,8 @@
 <template>
-  <isp-manage></isp-manage>
+  <div class="addr-isp-container">
+    <span class="addr-isp-header">{{ t('label.ISPPrefix') }}</span>
+    <isp-manage style="width: 50%"></isp-manage>
+  </div>
 </template>
 
 <script setup>
@@ -9,4 +12,20 @@ const { t } = useI18n()
 </script>
 
 <style scoped>
+.addr-isp-container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.addr-isp-header{
+  width: 90%;
+  margin: 0 auto 30px;
+  padding: 20px 10px 10px;
+  font-size: 20px;
+  font-weight: bold;
+  color: #343a40;
+  font-family:'Times New Roman', Times, serif, Georgia,"Microsoft YaHei",sans-serif;
+  text-align: left;
+  border-bottom: 1px solid #eeeeee;
+}
 </style>
